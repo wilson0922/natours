96f457e98307453f3a0ecf5de19460316c4cdf1b
tree70373 <tree70373@gmail.com>
2023-05-11 18:46:13 +0800

    https setting

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,17 +18,16 @@ const signToken = id => {
   )
 }
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id)
-  const cookieOptions = {
+
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-  }
-
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
-  res.cookie('jwt', token, cookieOptions)
+    secure: req.secure || req.headers('x-forwarded-proto') === 'https',
+  })
 
   // Remove password from output
   user.password = undefined
@@ -56,7 +55,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   // console.log(url)
   await new Email(newUser, url).sendWelcome()
 
-  createSendToken(newUser, 201, res)
+  createSendToken(newUser, 201, req, res)
 })
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -76,7 +75,7 @@ exports.login = catchAsync(async (req, res, next) => {
   // ('abcd1234'==='$2a$12$6eAvy0dNx/svtVN9oQBrJOlcD5fn6UX.NN6pii6mFAIJBROewb7tK')
 
   //   3) If everything is ok, send token to client
-  createSendToken(user, 200, res)
+  createSendToken(user, 200, req, res)
 })
 
 exports.logout = (req, res) => {
@@ -227,7 +226,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   // 3) Update changedPasswordAt property for the user
   // 4) Log the user in, sent JWT
-  createSendToken(user, 200, res)
+  createSendToken(user, 200, req, res)
 })
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -244,5 +243,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   // User.findByIdAndUpdate will not work as intended
 
   // 4) Log user in, send JWT
-  createSendToken(user, 200, res)
+  createSendToken(user, 200, req, res)
 })
