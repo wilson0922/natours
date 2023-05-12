7b225a1e503000c0dba5f22e5d8497b53454afa3
tree70373 <tree70373@gmail.com>
2023-05-12 08:44:11 +0800

    implement Cors and add node version

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean')
 const hpp = require('hpp')
 const cookieParser = require('cookie-parser')
 const compression = require('compression')
+const cors = require('cors')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -25,6 +26,12 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 // 1) GLOBAL MIDDLEWARES
+// Implemant CORS
+app.use(cors())
+// Accesss-control-allow-origin
+app.options('*', cors())
+app.options('/api/v1/tours/:id', cors())
+
 // Serving Static files
 app.use(express.static(path.join(__dirname, 'public')))
 
