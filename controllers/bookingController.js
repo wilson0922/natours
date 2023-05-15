61ec194038e164fd27d26d09defe450d53a6e398
tree70373 <tree70373@gmail.com>
2023-05-15 16:36:56 +0800

    webhook debug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -5,7 +5,7 @@ const Booking = require('../models/bookingModel')
 const catchAsync = require('../utils/catchAsync')
 const factory = require('./handlerFactory')
 
-exports.getCheckoutSession = catchAsyncError(async (req, res, next) => {
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId)
   // 2) Create checkout session
