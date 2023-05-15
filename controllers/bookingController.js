ceedce16e28e080c5267b4d587088359f3e81682
tree70373 <tree70373@gmail.com>
2023-05-15 16:16:42 +0800

    webhook debug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -14,9 +14,7 @@ exports.getCheckoutSession = catchAsyncError(async (req, res, next) => {
     session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       mode: 'payment',
-      success_url: `${req.protocol}://${req.get(
-        'host'
-      )}/my-tours?alert=booking`,
+      success_url: `${req.protocol}://${req.get('host')}/my-tours`,
       cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
       customer_email: req.user.email,
       client_reference_id: req.params.tourId,
