695cf57e579a3c75c7be6737a9b1cc47aa6f293d
tree70373 <tree70373@gmail.com>
2023-05-15 14:54:15 +0800

    webhook debug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,43 +1,42 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const Tour = require('../models/tourModel')
-const User = require('../models/userModel')
 const Booking = require('../models/bookingModel')
 const catchAsync = require('../utils/catchAsync')
 const factory = require('./handlerFactory')
+const User = require('../models/userModel')
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId)
 
-  const transformedItems = [
-    {
-      quantity: 1,
-      price_data: {
-        currency: 'usd',
-        unit_amount: tour.price * 100,
-        product_data: {
-          name: `${tour.name} Tour`,
-          description: tour.summary,
-          images: [
-            `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-          ],
-        },
-      },
-    },
-  ]
-
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/?tour=${
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
-    line_items: transformedItems,
-    mode: 'payment',
+    line_items: [
+      {
+        quantity: 1,
+        name: `${tour.name} Tour`,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
+          },
+        },
+      },
+    ],
   })
 
   // 3) Create session as response
@@ -48,22 +47,29 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 })
 
 // exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   //Temporary solution
-//   const { tour, user, price } = req.query
-//   if (!tour && !user && !price) return next()
-//   await Booking.create({ tour, user, price })
-//   res.redirect(req.originalUrl.split('?')[0])
-// })
+//   // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
+//   const { tour, user, price } = req.query;
+
+//   if (!tour && !user && !price) return next();
+//   await Booking.create({ tour, user, price });
+
+//   res.redirect(req.originalUrl.split('?')[0]);
+// });
+
+// /webhook-checkout
+// checkout.session.completed
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id
-  const user = (await User.findOne({ email: session.customer_email })).id
-  const price = session.line_items[0].price_data.unit_amount / 100
+  const user = (await User.findOne({ email: session.customer_email }))._id
+  const price = session.amount_total / 100
+
   await Booking.create({ tour, user, price })
 }
 
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature']
+
   let event
   try {
     event = stripe.webhooks.constructEvent(
@@ -72,11 +78,12 @@ exports.webhookCheckout = (req, res, next) => {
       process.env.STRIPE_WEBHOOK_SECRET
     )
   } catch (err) {
-    return res.status(400).send(`Webhook Error: ${err.message}`)
+    return res.status(400).send(`Webhook error: ${err.message}`)
   }
-  if (event.type === 'checkout.session.completed') {
+
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object)
-  }
+
   res.status(200).json({ received: true })
 }
 
